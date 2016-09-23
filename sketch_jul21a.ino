07c91e196ebf7caffb41b8922f0943824f6f0d55
Artem Kramov <artemkramov@gmail.com>
2016-09-23 15:24:53 +0300

    Signal receive

--- sketch_jul21a.ino
+++ sketch_jul21a.ino
@@ -1,5 +1,12 @@
 #define LED 13
-#define RX_PIN A3
+#define RX_PIN A0
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
  
 /* Toggle HIGH or LOW digital write */
 volatile int toggle = 0;
@@ -7,6 +14,10 @@ volatile int toggle = 0;
 volatile int sampleReady = 0;
 
 volatile int sample = 0;
+
+volatile int samples = 0;
+
+volatile int samplesCount = 7;
 
 int upperThreshold = 70;
 
@@ -21,7 +32,6 @@ void initRX(void)
     OCR1A = 250;
     
     TCCR1B |= (1 << CS11);
-    TCCR1B |= (1 << CS10);
     
     // turn on CTC mode:
     TCCR1B |= (1 << WGM12);
@@ -38,8 +48,12 @@ void setup(){
 }
 
 void loop(){
-  while (sampleReady) {
-    Serial.println(sample);   
+  if (sampleReady) {
+    digitalWrite(LED, sample == 0 ? LOW : HIGH);
+    if (sample == 1) {
+      Serial.println("--------BEAT----------");
+    }
+    sampleReady = 0;
   }
 }
 
@@ -48,10 +62,8 @@ ISR(TIMER1_COMPA_vect)
 {
    cli();
    int rawData = analogRead(RX_PIN);
-   if (rawData < lowerThreshold || rawData > upperThreshold) {
+   if (!sampleReady && rawData < lowerThreshold || rawData > upperThreshold) {
        sample = rawData > upperThreshold ? 1 : 0;
-       digitalWrite(LED, toggle == 0 ? HIGH : LOW);
-       toggle = ~toggle;
        sampleReady = 1;
    }
    sei();
